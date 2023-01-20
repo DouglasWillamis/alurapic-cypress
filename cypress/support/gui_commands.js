31256f9735e4af41029ab91067345e2126ecd496
Douglas Willamis <douglaswillamis@gmail.com>
2023-01-20 17:16:58 -0300

    feat: adicionando comando para registra

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -4,3 +4,13 @@ Cypress.Commands.add('login', (username, senha) => {
     cy.get('input[formcontrolname="password"]').type(senha)
     cy.get('button[type="submit"]').click()
 })
+
+Cypress.Commands.add('registra', (email, fullName, userName, password) => {
+    cy.contains('a', 'Register now').click()
+    cy.contains('button', 'Register').click()
+    cy.get('input[formcontrolname="email"]').type(email)
+    cy.get('input[formcontrolname="fullName"]').type(fullName)
+    cy.get('input[formcontrolname="userName"]').type(userName)
+    cy.get('input[formcontrolname="password"]').type(password)
+    cy.contains('button', 'Register').click()
+})
