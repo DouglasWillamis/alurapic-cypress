dd632e0fd0fc9eee9545b15b8c3f582f19950450
Douglas Willamis <douglaswillamis@gmail.com>
2023-01-23 11:02:14 -0300

    feat: api login com sucesso

---
+++ cypress/e2e/login-api.cy.js
@@ -0,0 +1,16 @@
+describe('Login de usuário no alura pic', () => {
+    it('fazer login do flavio', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then((response) => {
+            expect(response.status).to.be.equal(200)
+            expect(response.body).is.not.empty
+            expect(response.body).to.have.property('id')
+            expect(response.body.id).to.be.equal(1)
+            expect(response.body).to.have.property('email')
+            expect(response.body.email).to.be.equal('flavio@alurapic.com.br')
+        })
+    })
+})
