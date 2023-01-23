c1acf9f36d566ddbe1c7cb8c8bc7e2e4b7fbdd2a
Douglas Willamis <douglaswillamis@gmail.com>
2023-01-23 11:34:59 -0300

    chore: adicionando baseUrl ao arquivo de configuração do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://alura-fotos.herokuapp.com",
+    viewportWidth: 1440,
+    viewportHeight: 900,
     specPattern: ['cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', 'cypress/e2e/**/*.{js,jsx,ts,tsx}']
   },
   reporter: "mochawesome",
