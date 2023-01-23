57b04f484c3824e5345e76ea580c403bf457e292
Douglas Willamis <douglaswillamis@gmail.com>
2023-01-23 12:48:03 -0300

    refacator: ocultando senha ao realizar login no log

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', (username, senha) => {
     cy.get('input[formcontrolname="userName"]').type(username)
-    cy.get('input[formcontrolname="password"]').type(senha)
+    cy.get('input[formcontrolname="password"]').type(senha, {log: false})
     cy.get('button[type="submit"]').click()
 })
 
