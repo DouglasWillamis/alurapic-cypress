b5dc9743e9a2785d53912753999e32fa153ad679
Douglas Willamis <douglaswillamis@gmail.com>
2023-01-23 12:26:11 -0300

    refactor: utilizando parâmetros nomeados com valores padrão no comando cadastro

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,16 +1,20 @@
-
 Cypress.Commands.add('login', (username, senha) => {
     cy.get('input[formcontrolname="userName"]').type(username)
     cy.get('input[formcontrolname="password"]').type(senha)
     cy.get('button[type="submit"]').click()
 })
 
-Cypress.Commands.add('registra', (email, fullName, userName, password) => {
+Cypress.Commands.add('registra', ({
+        email = '{backspace}',
+        fullname = '{backspace}',
+        username = '{backspace}',
+        password = '{backspace}'
+    } = {}) => {
     cy.contains('a', 'Register now').click()
     cy.contains('button', 'Register').click()
     cy.get('input[formcontrolname="email"]').type(email)
-    cy.get('input[formcontrolname="fullName"]').type(fullName)
-    cy.get('input[formcontrolname="userName"]').type(userName)
+    cy.get('input[formcontrolname="fullName"]').type(fullname)
+    cy.get('input[formcontrolname="userName"]').type(username)
     cy.get('input[formcontrolname="password"]').type(password)
     cy.contains('button', 'Register').click()
 })
